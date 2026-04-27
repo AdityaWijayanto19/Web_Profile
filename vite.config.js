c592ca183923a70a8c46baa83c85281c920b0d7a
AdityaWijayanto19 <adityaputraw49@student.ub.ac.id>
2026-04-28 05:25:26 +0700

    fix: add education.js to vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/dashboard.js',
-                'resources/js/profile.js',
-                'resources/js/sections/education.js'
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/dashboard.js",
+                "resources/js/profile.js",
+                ...glob.sync("resources/js/sections/*.js"),
             ],
             refresh: true,
         }),
